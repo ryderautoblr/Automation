1a1fbfbcd3f2c67c436893bf2ec6b45de97ece01
ryderautoblr <ryderautoblr@gmail.com>
2021-08-05 00:25:33 +0530

    merging with app data start

--- Bizmate/busyToAppPanel.py
+++ Bizmate/busyToAppPanel.py
@@ -1,7 +1,7 @@
 import wx
 import os
 import createSelectFilePanel
-import xlUtils
+import pandas
 import wx.lib.scrolledpanel as scrolled
 
 class busyToAppPanel(scrolled.ScrolledPanel):
@@ -11,40 +11,35 @@ class busyToAppPanel(scrolled.ScrolledPanel):
 
     self.selectBusyList = createSelectFilePanel.createSelectFilePanel(self,'Select Busy Items Files',0,0)
     self.selectAppList = createSelectFilePanel.createSelectFilePanel(self,'Select App Items Files',0,100)   
-    self.selectAppSample = createSelectFilePanel.createSelectFilePanel(self,'Select App Sample Files',0,200)   
-    self.selectKeywords = createSelectFilePanel.createSelectFilePanel(self,'Select Keywords Files',0,300)   
-
-    self.btnCheckCategories = wx.Button(self, label='Check Categories', pos=(10, 400))
+    
+    self.btnCheckCategories = wx.Button(self, label='Check Categories', pos=(10, 200))
     self.btnCheckCategories.Bind(wx.EVT_BUTTON, self.on_check_categories)
 
-    self.btnUpdateAppFiles = wx.Button(self, label='Update App Files', pos=(200, 400))
+    self.btnUpdateAppFiles = wx.Button(self, label='Update App Files', pos=(200, 200))
     self.btnUpdateAppFiles.Bind(wx.EVT_BUTTON, self.on_update_app)
     self.btnUpdateAppFiles.Disable()
 
-    self.textLog = wx.TextCtrl(self, pos=(10, 450),size=(800,200), style=wx.TE_MULTILINE)
+    self.textLog = wx.TextCtrl(self, pos=(10, 250),size=(800,200), style=wx.TE_MULTILINE)
     self.busyData = []
 
   def on_check_categories(self,event):
-    xlObj = xlUtils.xlUtils()
-    
-    busyCategoryCol = ord('C') - ord('A')
     busyFileLoc = self.selectBusyList.getFileName()
-    busySheet = xlObj.getSheet(busyFileLoc,0)
-    self.busyData = xlObj.getData(7,busySheet)
+    self.busyDf = pandas.read_excel(busyFileLoc)
     busyCategorySet = set()
 
-    for data in self.busyData[busyCategoryCol]:
+    for data in self.busyDf["Parent Group"]:
+      if pandas.isnull(data): continue
       if data.strip():
         busyCategorySet.add(data)
 
     appCategoryCol = ord('B') - ord('A')
     appListFileLoc = self.selectAppList.getFileName()
-    appCategorySheet = xlObj.getSheet(appListFileLoc,1)
-    appCategoryData = xlObj.getData(1,appCategorySheet)
+    self.appDf = pandas.read_excel(appListFileLoc,None)
+    appCategoryData = self.appDf["Category Details"]["Category"].to_list()
     
     missingCategories = []
     for data in sorted(busyCategorySet):
-      if data not in appCategoryData[appCategoryCol]:
+      if data not in appCategoryData:
         missingCategories.append(data)
 
     logDetails = ""
@@ -59,6 +54,10 @@ class busyToAppPanel(scrolled.ScrolledPanel):
     self.textLog.SetValue(logDetails)
 
   def on_update_app(self,event):
-    x = 1
+    self.appProductsDf = self.appDf["Product Details"]
+    self.appProductsDf["Long Name"] = self.appProductsDf["Product Name"]
+    self.newDf = pandas.merge(self.busyDf,self.appProductsDf,on=["Long Name"],how="outer")
+    print (self.newDf,self.busyDf,self.appProductsDf)
+
 
 
